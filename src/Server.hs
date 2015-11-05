8e09ca19e5c2856ff3e23301314f21946f46040c
Robin Schroer <sulami@peerwire.org>
2015-11-05 23:53:46 +0100

    add a tiny bit of docs

--- src/Server.hs
+++ src/Server.hs
@@ -4,6 +4,7 @@ module Server (
 
 import           Control.Monad (when)
 
+-- | Run the server on the specified port.
 runServer :: Bool -> Word -> IO ()
 runServer debug port = do
   when debug . putStrLn $ "Listening on port " ++ show port
