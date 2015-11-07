923e6dbb21129799529724141eac9e1f23941b11
Robin Schroer <sulami@peerwire.org>
2015-11-07 05:21:32 +0100

    extract pure versions of insert and lookup for testing

--- src/LimitedHashMap.hs
+++ src/LimitedHashMap.hs
@@ -25,22 +25,28 @@ initialState msize = LimitedHashMap HML.empty msize []
 
 -- | Insert a new KVP
 insert :: ByteString -> ByteString -> LHM ()
-insert k v = do
-  state <- get
-  let currentSize = HML.size $ state^.hashMap
-      isFull = currentSize == state^.maxSize
-      alreadyMember = HML.member k $ state^.hashMap
+insert k v = modify $ insert' k v
+
+-- | Pure version of 'insert' for testing
+insert' :: ByteString -> ByteString -> LimitedHashMap -> LimitedHashMap
+insert' k v s =
+  let currentSize = HML.size $ s^.hashMap
+      isFull = currentSize == s^.maxSize
+      alreadyMember = HML.member k $ s^.hashMap
       needsDeletion = isFull && not alreadyMember
-      delCandidate = head $ state^.used
-  modify $
-    (hashMap %~ (HML.insert k v)) .
-    (if needsDeletion then used %~ tail else id) .
-    (if needsDeletion then hashMap %~ (HML.delete delCandidate) else id) .
-    (used %~ (++ [k]))
+      delCandidate = head $ s^.used
+  in hashMap %~ (HML.insert k v) $
+     (if needsDeletion then used %~ tail else id) $
+     (if needsDeletion then hashMap %~ (HML.delete delCandidate) else id) $
+     (used %~ (++ [k])) s
 
 -- | Query a value for a key
 query :: ByteString -> LHM (Maybe ByteString)
 query k = do
   state <- get
-  return . HML.lookup k $ state^.hashMap
+  return $ query' k state
+
+-- | Pure version of 'query' for testing
+query' :: ByteString -> LimitedHashMap -> Maybe ByteString
+query' k s = HML.lookup k $ s^.hashMap
 
