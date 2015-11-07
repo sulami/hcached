df5540a99db49fbfbe81c0739ba1a2bc3fc3bd38
Robin Schroer <sulami@peerwire.org>
2015-11-07 03:19:13 +0100

    add the logic to add new kvps

--- src/HashTable.hs
+++ src/HashTable.hs
@@ -3,15 +3,15 @@
 module HashTable (
 ) where
 
-import           Control.Lens ((^.), makeLenses)
-import           Control.Monad.State (State, get)
+import           Control.Lens ((^.), (%~), makeLenses)
+import           Control.Monad.State (State, get, modify)
 import           Data.ByteString (ByteString)
 import qualified Data.HashMap.Lazy as HML
 
 -- | Stateful data needed for the size-limited hashtable
 data LimitedHashMap = LimitedHashMap
   { _hashMap :: !(HML.HashMap ByteString ByteString) -- ^ The Hashmap used
-  , _maxSize :: !Integer                             -- ^ Maximum hashmap size
+  , _maxSize :: !Int                                 -- ^ Maximum hashmap size
   , _used    :: ![ByteString]                        -- ^ Recently used hashes
   }
 
@@ -21,6 +21,21 @@ makeLenses ''LimitedHashMap
 type LHM a = State LimitedHashMap a
 
 -- | The inital state to use when starting up
-initalState :: Integer -> LimitedHashMap
+initalState :: Int -> LimitedHashMap
 initalState msize = LimitedHashMap HML.empty msize []
+
+-- | Insert a new KVP
+insert :: ByteString -> ByteString -> LHM ()
+insert k v = do
+  state <- get
+  let currentSize = HML.size $ state^.hashMap
+      isFull = currentSize == state^.maxSize
+      alreadyMember = HML.member k $ state^.hashMap
+      needsDeletion = isFull && not alreadyMember
+      delCandidate = head $ state^.used
+  modify $
+    (hashMap %~ (HML.insert k v)) .
+    (if needsDeletion then used %~ tail else id) .
+    (if needsDeletion then hashMap %~ (HML.delete delCandidate) else id) .
+    (used %~ (++ [k]))
 
