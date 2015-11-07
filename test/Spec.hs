15e0abe986dda4019ccc976d0ad90cbeb783f09e
Robin Schroer <sulami@peerwire.org>
2015-11-07 05:35:21 +0100

    moar tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Monad.State
+
 import           Control.Lens
 import           Test.Hspec
 
@@ -10,4 +14,12 @@ main = hspec $ do
       (initialState 10)^.maxSize `shouldBe` 10
     it "sets the proper initial used list" $ do
       (initialState 10)^.used `shouldBe` []
+    it "can insert a key-value-pair" $ do
+      (query' "1" $ insert' "1" "one" $ initialState 10) `shouldBe` (Just "one")
+    it "recognizes non-existent keys" $ do
+      (query' "2" $ insert' "1" "one" $ initialState 10) `shouldBe` Nothing
+    it "should only save the specified amount of KVPs" $ do
+      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState 1
+      query' "1" hm `shouldBe` Nothing
+      query' "2" hm `shouldBe` (Just "two")
 
