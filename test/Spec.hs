33926969ce4a88195a610b373180e42ad18f06d3
Robin Schroer <sulami@peerwire.org>
2015-11-08 11:51:21 +0100

    fix the tests to run again

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,24 +12,26 @@ main :: IO ()
 main = hspec $ do
   describe "LimitedHashMap" $ do
     it "sets the proper initial maximum size" $ do
-      (initialState 10)^.maxSize `shouldBe` 10
+      (initialState False 10)^.maxSize `shouldBe` 10
     it "sets the proper initial used list" $ do
-      (initialState 10)^.mru `shouldBe` []
+      (initialState False 10)^.mru `shouldBe` []
     it "can insert a key-value-pair" $ do
-      (query' "1" $ insert' "1" "one" $ initialState 10) `shouldBe` (Just "one")
+      let hm = insert' "1" "one" $ initialState False 10
+      query' "1" hm `shouldBe` (Just "one")
     it "recognizes non-existent keys" $ do
-      (query' "2" $ insert' "1" "one" $ initialState 10) `shouldBe` Nothing
+      let hm = insert' "1" "one" $ initialState False 10
+      query' "2" hm `shouldBe` Nothing
     it "only saves the specified amount of KVPs" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState 1
+      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 1
       HML.size (hm^.hashMap) `shouldBe` 1
     it "deletes the first inserted key when full" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState 1
+      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 1
       query' "1" hm `shouldBe` Nothing
       query' "2" hm `shouldBe` (Just "two")
     it "deletes the deleted key from the mru list" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState 1
+      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 1
       hm^.mru `shouldBe` ["2"]
     it "updates the most recently used list to reflect queries" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState 2
+      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 2
       (queried' "1" hm)^.mru `shouldBe` ["1", "2"]
 
