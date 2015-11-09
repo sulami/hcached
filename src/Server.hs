26f62c4263633ca3e834177d9a695239d3997e38
Robin Schroer <sulami@peerwire.org>
2015-11-09 22:23:41 +0100

    write an answer function to append the newlines

--- src/Server.hs
+++ src/Server.hs
@@ -42,27 +42,31 @@ parse lhm sock msg = do
   case head cmds of
     "set" -> parseSet lhm sock $ drop 1 cmds
     "get" -> parseGet lhm sock $ drop 1 cmds
-    _     -> send sock $ C8.unwords ["ERROR unknown command", head cmds, "\r\n"]
+    _     -> answer sock $ C8.unwords ["ERROR unknown command", head cmds]
 
 -- | Set a key-value-pair
 parseSet :: MVar LimitedHashMap -> Socket -> [C8.ByteString] -> IO ()
 parseSet lhm sock msg = do
   if length msg < 2
-    then send sock "CLIENT_ERROR\r\n"
+    then answer sock "CLIENT_ERROR"
     else do
       set lhm (head msg) (C8.unwords $ drop 1 msg)
-      send sock "STORED\r\n"
+      answer sock "STORED"
 
 -- | Get a value for a key
 parseGet :: MVar LimitedHashMap -> Socket -> [C8.ByteString] -> IO ()
 parseGet lhm sock msg = do
   if length msg /= 1
-    then send sock "CLIENT_ERROR\r\n"
+    then answer sock "CLIENT_ERROR"
     else do
       rv <- get lhm $ head msg
       case rv of
-        Nothing  -> send sock "NOT_FOUND\r\n"
-        Just val -> send sock $ C8.unwords ["VALUE", val, "\r\n"]
+        Nothing  -> answer sock "NOT_FOUND"
+        Just val -> answer sock $ C8.unwords ["VALUE", val]
+
+-- | Write an answer to a socket. Appends the correct line-ending
+answer :: Socket -> C8.ByteString -> IO ()
+answer sock msg = send sock $ C8.concat [msg, "\r\n"]
 
 -- | Print debug output if enabled
 debugP :: MVar LimitedHashMap -> String -> IO ()
