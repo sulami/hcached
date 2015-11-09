64ca53f4c83439c2058caaaa5406eadf78df5f1e
Robin Schroer <sulami@peerwire.org>
2015-11-09 14:47:27 +0100

    fix the tests to work again with the new IO stuff

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Control.Monad.State
-
 import           Control.Lens
 import           Test.Hspec
 import qualified Data.HashMap.Lazy as HML
@@ -16,22 +14,27 @@ main = hspec $ do
     it "sets the proper initial used list" $ do
       (initialState False 10)^.mru `shouldBe` []
     it "can insert a key-value-pair" $ do
-      let hm = insert' "1" "one" $ initialState False 10
+      hm <- insert' "1" "one" $ initialState False 10
       query' "1" hm `shouldBe` (Just "one")
     it "recognizes non-existent keys" $ do
-      let hm = insert' "1" "one" $ initialState False 10
+      hm <- insert' "1" "one" $ initialState False 10
       query' "2" hm `shouldBe` Nothing
     it "only saves the specified amount of KVPs" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 1
+      hm0 <- insert' "1" "one" $ initialState False 1
+      hm <- insert' "2" "two" hm0
       HML.size (hm^.hashMap) `shouldBe` 1
     it "deletes the first inserted key when full" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 1
+      hm0 <- insert' "1" "one" $ initialState False 1
+      hm <- insert' "2" "two" hm0
       query' "1" hm `shouldBe` Nothing
       query' "2" hm `shouldBe` (Just "two")
     it "deletes the deleted key from the mru list" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 1
+      hm0 <- insert' "1" "one" $ initialState False 1
+      hm <- insert' "2" "two" hm0
       hm^.mru `shouldBe` ["2"]
     it "updates the most recently used list to reflect queries" $ do
-      let hm = insert' "2" "two" $ insert' "1" "one" $ initialState False 2
-      (queried' "1" hm)^.mru `shouldBe` ["1", "2"]
+      hm0 <- insert' "1" "one" $ initialState False 2
+      hm <- insert' "2" "two" hm0
+      rv <- queried' "1" hm
+      rv^.mru `shouldBe` ["1", "2"]
 
