c6133d8ea242da09422afc4f5873382450910ae2
Robin Schroer <sulami@peerwire.org>
2015-11-22 22:35:58 +0100

    add testing for the janitor

--- test/ServerSpec.hs
+++ test/ServerSpec.hs
@@ -1,21 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ServerSpec where
 
-import           Control.Concurrent (forkIO)
+import           Control.Concurrent (forkIO, threadDelay)
+import           Control.Concurrent.MVar (newMVar, readMVar)
+import           Data.Maybe (isJust)
 import           System.IO (BufferMode (..), hGetLine, hPutStr, hSetBuffering)
 
+import           Control.Lens (view)
 import           Network (PortID (..), connectTo)
 import           Test.Hspec
 
-import           Server (initialState, runServer)
+import           LimitedHashMap (get', set)
+import           Server (initialState, janitor, lhm, runServer)
 
 spec :: Spec
 spec = do
-  istate <- runIO $ initialState False 60 3
-  runIO . forkIO $ runServer istate 11212
+  istate <- runIO $ initialState False 1 3
 
-  it "answers to requests on its port" $ do
-    handle <- connectTo "localhost" $ PortNumber 11212
-    hSetBuffering handle LineBuffering
-    hPutStr handle "get key\n"
-    hGetLine handle `shouldReturn` "NOT_FOUND\r"
+  describe "Exposed Interface" $ do
+    runIO . forkIO $ runServer istate 11212
+
+    it "answers to requests on its port" $ do
+      handle <- connectTo "localhost" $ PortNumber 11212
+      hSetBuffering handle LineBuffering
+      hPutStr handle "get key\n"
+      hGetLine handle `shouldReturn` "NOT_FOUND\r"
+
+  describe "Janitor" $ do
+    mst <- runIO $ newMVar istate
+    runIO . forkIO $ janitor mst
+
+    it "cleans up regularly" $ do
+      mlhm <- view lhm <$> readMVar mst
+      set mlhm "one" "1" (-1)
+      hmBefore <- readMVar mlhm
+      get' hmBefore "one" `shouldSatisfy` isJust
+      threadDelay 1000000 -- wait a second
+      hmAfter <- readMVar mlhm
+      get' hmAfter "one" `shouldBe` Nothing
 
